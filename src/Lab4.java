0bd40fd0ed8b8420c9cdab08c8377274ccab3275
virginia <mavirginia.cordero@gmail.com>
2018-07-18 17:29:12 -0400

    this is my third lab, I cannot believe I finished it before 5pm

---
+++ src/Lab4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Lab4 {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+
+		
+		String userInput;
+		do {
+			System.out.println("Please enter an integer:");
+			int userInt = scnr.nextInt();
+			for (int i = 1; i <= userInt; i++) {
+				System.out.printf("%-10s %-10s %-10s\n", i, Math.pow(i, 2), Math.pow(i, 3));
+			}
+				System.out.println("Would you like to continue? (y/n)");
+				userInput = scnr.next();		
+			} while (userInput.equals("y"));
+
+		System.out.println("Goodbye.");
+
+	}
+}
